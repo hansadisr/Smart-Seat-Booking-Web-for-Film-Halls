574e960d6df5bd166b29cbceed87b67d29f574a5
Hansadee Rathnayaka <hansadirathnayaka@gmail.com>
2025-08-12 12:17:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
@@ -11,18 +12,20 @@ import Movies from './pages/Movies';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/movie" element={<Movie />} />
-        <Route path="/booking" element={<Booking />} />
-        <Route path="/bookingList" element={<BookingList />} />
-        <Route path="/location" element={<Location />} />
-        <Route path="/movies" element={<Movies />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/movie/:id" element={<Movie />} />
+          <Route path="/booking/:movieId" element={<Booking />} />
+          <Route path="/bookingList" element={<BookingList />} />
+          <Route path="/location" element={<Location />} />
+          <Route path="/movies" element={<Movies />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
